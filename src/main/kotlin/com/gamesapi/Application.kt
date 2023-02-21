a0dff996ee3c72dc7ce21d3e2744cd009b67e2cd
TudorBozu <tudorbozu9@gmail.com>
2023-02-21 21:22:15 +0200

    Ktor

---
+++ src/main/kotlin/com/gamesapi/Application.kt
@@ -0,0 +1,15 @@
+package com.gamesapi
+
+import io.ktor.server.application.*
+import com.gamesapi.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureMonitoring()
+    configureSerialization()
+    configureSecurity()
+    configureRouting()
+}
